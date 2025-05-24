4b62eea68dfceee2be3033aa81571a8487e09886
bobbybotbop <bobbybotbop@gmail.com>
2025-05-24 19:43:47 -0400

    finished DX and can run both the electron and react dev servers in parallel with one command

--- src/Electron/main.ts
+++ src/Electron/main.ts
@@ -1,9 +1,15 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
 
 type test = string;
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  //quick reloading if dev
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5101/");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
