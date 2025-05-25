68ea02d3a13d004c2752cb2f53e0d74c4287db06
bobbybotbop <bobbybotbop@gmail.com>
2025-05-24 21:47:18 -0400

    worked on context bridge

--- src/Electron/main.ts
+++ src/Electron/main.ts
@@ -2,11 +2,18 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
 import { pollResources } from "./resourceManager.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 type test = string;
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      //path to a file
+      preload: getPreloadPath(),
+    },
+  });
+
   //quick reloading if dev
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5101/");
--- src/Electron/resourceManager.ts
+++ src/Electron/resourceManager.ts
@@ -1,12 +1,48 @@
 import osUtils from "os-utils";
+import os from "os";
+import fs from "fs";
 
 const POLLING_INTERVAL = 500;
 
 export function pollResources() {
-  setInterval(() => {
-    getCpuUsage();
+  setInterval(async () => {
+    const cpuUsage = await getCpuUsage();
+    const ramUsage = getRamUsage();
+    const storageData = getStorageData();
+    console.log({ cpuUsage, ramUsage, storageUsage: storageData.usage });
   }, POLLING_INTERVAL);
 }
+
+export function getStaticData() {
+  const totalStorage = getStorageData().total;
+  const cpuModel = os.cpus()[0].model;
+  //total mems is in mb
+  const totalMemGB = Math.floor(osUtils.totalmem() / 1024);
+
+  return {
+    totalStorage,
+    cpuModel,
+    totalMemGB,
+  };
+}
+
 function getCpuUsage() {
-  osUtils.cpuUsage((percent) => console.log(percent));
+  return new Promise((percent) => osUtils.cpuUsage(percent));
+}
+
+//.osUtils.freemempercentage() is a sychronious function so you dont need a promise
+function getRamUsage() {
+  return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  //gives synchronous data so we dont need a promise
+  const s = fs.statfsSync(process.platform === "win32" ? "C://" : "/");
+  const tot = s.bsize * s.blocks;
+  const free = s.bsize * s.bfree;
+
+  return {
+    total: Math.floor(tot / 1_000_000_000),
+    usage: 1 - free / tot,
+  };
 }
